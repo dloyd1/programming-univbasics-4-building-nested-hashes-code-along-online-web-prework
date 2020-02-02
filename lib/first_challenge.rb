3c0e324de7f41834d850190e226890a544e5a395
Daniel Loyd <dhloyd@icloud.com>
2020-02-02 10:26:56 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -4,13 +4,13 @@ def first_challenge
        patriarch: {},
        matriarch: {},
        hero: {},
-       hero_friends: {}
+       hero_friends: []
      },
      :Capulet => {
        patriarch: {},
        matriarch: {},
        heroine: {},
-       heroine_friends: {}
+       heroine_friends: []
      }
    }
 end
