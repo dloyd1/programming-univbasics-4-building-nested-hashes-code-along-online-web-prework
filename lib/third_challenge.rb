f7b56dd268b35f4daf420d2b39f93c61dadec5a8
Daniel Loyd <dhloyd@icloud.com>
2020-02-02 10:32:40 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,12 +7,12 @@ def third_challenge
        hero_friends: []
      },
      :capulet => {
-       patriarch: {},
-       matriarch: {},
-       heroine: {},
+       patriarch: {name: "Lord Capulet", age: "50"},
+       matriarch: {name: "Lady Capulet", age: "51"},
+       heroine: {name: "Juliet", age: "15", status: "alive"}
        heroine_friends: []
      }
    }
 
-  
+
 end
